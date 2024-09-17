77f0192d00115aae74772957d0389d6b0aa0264e
Ramendra Bharti <iamramendra.tech@gmail.com>
2024-09-17 22:31:07 +0530

    Update main.tf

--- Day-01-Terraform/002-Project-Create-Multiple-EC2-Instances/main.tf
+++ Day-01-Terraform/002-Project-Create-Multiple-EC2-Instances/main.tf
@@ -1,1 +1,10 @@
+provide "aws" {
 
+region = "us-east-1"
+}
+
+resources "aws_instance" "example" {
+    ami = "ami-ami-0c55b159cbfafe1f0"
+   instance_type = "t2.micro"
+   count = 2   #for multiple instance we can use count keyword.
+}
